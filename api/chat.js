e8eb8b82f58fe701ac3c68a32520f03e7b17d4f8
etpsch2802-hash <etpsch2802@gmail.com>
2026-05-25 15:54:57 +0200

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -1,4 +1,4 @@
-import { GoogleGenAI } from '@google/genai';
+import { GoogleGenAI } from 'https://esm.sh/@google/genai';
 
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
